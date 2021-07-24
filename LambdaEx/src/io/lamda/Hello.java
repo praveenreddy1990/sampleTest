b0bb427481de1ea74b9fac0cd69d97f62599d2d1
praveenreddy1990 <spreddy.india@gmail.com>
2021-07-24 16:59:11 +0530

    Create Hello.java
    hello added

---
+++ LambdaEx/src/io/lamda/Hello.java
@@ -0,0 +1,12 @@
+package io.lamda;
+
+public class Hello implements Greeter {
+
+	@Override
+	public void perform() {
+		System.out.println("Hello World from Hello Greeter");
+		System.out.println("Hello World from Praveen");
+
+	}
+
+}

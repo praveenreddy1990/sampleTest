e88408270a71e3a8f7569489e825062eb96c9c39
praveenreddy1990 <spreddy.india@gmail.com>
2021-07-24 17:14:10 +0530

    hello greeter changed

--- LambdaEx/src/io/lamda/HelloGreeter.java
+++ LambdaEx/src/io/lamda/HelloGreeter.java
@@ -5,7 +5,8 @@ public class HelloGreeter implements Greeter {
 	@Override
 	public void perform() {
 		System.out.println("Hello World from Hello Greeter");
-		System.out.println("Hello World from Praveen");
+		System.out.println("Hello World from Praveen Reddy");
+		System.out.println("Hello World from Praveen Reddy");
 
 	}
 
